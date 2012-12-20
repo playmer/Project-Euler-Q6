923b6d49bfd4dcf29ecbb18a14bd6f30f70dcfb9
Joshua <playmer@gmail.com>
2012-12-20 13:53:50 -0500

    First Commit

--- Project Euler Q6/main.m
+++ Project Euler Q6/main.m
@@ -13,8 +13,31 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int sumOfSquares=0, squareOfSums=0, difference = 0;
+        
+        for (int i=1; i <= 100; i++) {
+            sumOfSquares += (i*i);
+            NSLog(@"Sum of Squares: %d", sumOfSquares);
+        }
+        
+        for (int i=1; i <= 100; i++) {
+            squareOfSums += i;
+            NSLog(@"Square of Sums: %d", squareOfSums);
+        }
+        squareOfSums = squareOfSums * squareOfSums;
+        NSLog(@"Square of Sums: %d", squareOfSums);        
+        
+        
+        // I suspect that there's a proof out there that suggests that the square of sums will always be greater than the sum of squares of some numbers 1-n. But I don't have time to prove it, or look for one, thus:
+        if (squareOfSums > sumOfSquares)
+        {
+            difference = squareOfSums - sumOfSquares;
+        }
+        else 
+        {
+            difference = sumOfSquares - squareOfSums;
+        }
+        NSLog(@"%d", difference);
         
     }
     return 0;
